57d69956bf07b9419f1d69448e535ea1f91173b9
Darrell Harmon <darrell@harmoninstruments.com>
2014-05-24 13:40:14 -0600

    change name to vna_dsp

--- pciesdr.c
+++ pciesdr.c
@@ -12,43 +12,43 @@
 #define SUCCESS 0
 #define DEVICE_NAME "vna_dsp"
 
-static struct pci_device_id pciesdr_pci_table[] = {
+static struct pci_device_id vna_dsp_pci_table[] = {
   {VENDOR_ID, DEVICE_ID, PCI_ANY_ID, PCI_ANY_ID, 0, 0 ,0},
   {0,}
 };
 
 static dma_addr_t dmahandle_buffer;
-static uint64_t *pciesdr_buffer;
+static uint64_t *vna_dsp_buffer;
 volatile static uint64_t *bar0base = 0;
-static int pciesdr_major = 0;
-static int pciesdr_is_open = 0;
+static int vna_dsp_major = 0;
+static int vna_dsp_is_open = 0;
 
-static int pciesdr_open(struct inode *inode, struct file *file){
-  if (pciesdr_is_open)
+static int vna_dsp_open(struct inode *inode, struct file *file){
+  if (vna_dsp_is_open)
     return -EBUSY;
-  pciesdr_is_open++;
+  vna_dsp_is_open++;
   try_module_get(THIS_MODULE); // increment the usage count
   return SUCCESS;
 }
 
-static int pciesdr_release(struct inode *inode, struct file *file){
-  pciesdr_is_open--;/* We're now ready for our next caller */
+static int vna_dsp_release(struct inode *inode, struct file *file){
+  vna_dsp_is_open--;/* We're now ready for our next caller */
   module_put(THIS_MODULE); // decrement the usage count
   return 0;
 }
 
-static ssize_t pciesdr_read(struct file *filp,/* see include/linux/fs.h   */
+static ssize_t vna_dsp_read(struct file *filp,/* see include/linux/fs.h   */
 			    char *buffer,/* buffer to fill with data */
 			    size_t length,/* length of the buffer     */
 			    loff_t * offset){
   unsigned long bytes_left;
   if(length > 4*1024*1024)
     length = 4*1024*1024;
-  bytes_left = copy_to_user(buffer, pciesdr_buffer, length);
+  bytes_left = copy_to_user(buffer, vna_dsp_buffer, length);
   return length - bytes_left;
 }
 
-static ssize_t pciesdr_write(struct file *filp,
+static ssize_t vna_dsp_write(struct file *filp,
 			    const char *buff,
 			    size_t len,
 			    loff_t * off){
@@ -58,10 +58,10 @@ static ssize_t pciesdr_write(struct file *filp,
 }
 
 static struct file_operations fops = {
- .read = pciesdr_read,
- .write = pciesdr_write,
- .open = pciesdr_open,
- .release = pciesdr_release
+ .read = vna_dsp_read,
+ .write = vna_dsp_write,
+ .open = vna_dsp_open,
+ .release = vna_dsp_release
 };
 
 static irqreturn_t vna_interrupt(int irq, void *dev_id, struct pt_regs *regs){
@@ -73,7 +73,7 @@ static irqreturn_t vna_interrupt(int irq, void *dev_id, struct pt_regs *regs){
 }
 
 
-static int pciesdr_probe(struct pci_dev *dev, const struct pci_device_id *id){
+static int vna_dsp_probe(struct pci_dev *dev, const struct pci_device_id *id){
   int i;
   int retval = pci_enable_device(dev);
   if(retval < 0) return retval;
@@ -85,13 +85,14 @@ static int pciesdr_probe(struct pci_dev *dev, const struct pci_device_id *id){
   pci_set_master  (dev);
   pci_set_dma_mask(dev, 0xFFFFFFFFFFFFFFFF); // if this fails, try 32
   pci_set_consistent_dma_mask(dev, 0xFFFFFFFFFFFFFFFF);
-  pci_enable_msi(dev); // check retval
+  if(pci_enable_msi(dev) < 0)
+    printk("VNA: pci_enable_msi() failed\n"); // check retval
   if(request_irq(dev->irq, (irq_handler_t) vna_interrupt, 0 /* flags */, DEVICE_NAME, dev) != 0)
     printk("VNA: request_irq() failed\n");
-  pciesdr_buffer = (uint64_t *) pci_alloc_consistent(dev, 4*1024*1024, &dmahandle_buffer);
-  if(pciesdr_buffer == NULL)
+  vna_dsp_buffer = (uint64_t *) pci_alloc_consistent(dev, 4*1024*1024, &dmahandle_buffer);
+  if(vna_dsp_buffer == NULL)
     printk("Failed to allocate rx buffer\n");
-  printk("Allocated a buffer at %llx %llx\n", (uint64_t) pciesdr_buffer, (uint64_t) dmahandle_buffer);
+  printk("Allocated a buffer at %llx %llx\n", (uint64_t) vna_dsp_buffer, (uint64_t) dmahandle_buffer);
   printk("Found PCI Express interface board\n");
   bar0base = (uint64_t *) ioremap(pci_resource_start(dev, 0), 65536);
   printk("pci_resource_start(dev, 0) = %llx\n", (uint64_t) pci_resource_start(dev, 0));
@@ -105,11 +106,11 @@ static int pciesdr_probe(struct pci_dev *dev, const struct pci_device_id *id){
     }
   writeq(0xDEADBEEF0BADC0DE, &bar0base[0]);
   for(i=0; i<256; i++)
-    pciesdr_buffer[i] = 0xBEEF00000000 | i;
+    vna_dsp_buffer[i] = 0xBEEF00000000 | i;
   for(i=0; i<8; i++)
     {
-      printk("dmabuf[%d] = %llx\n", i, (uint64_t) readq(&pciesdr_buffer[i]));
-      // pciesdr_buffer[i] = 0xDEAD;
+      printk("dmabuf[%d] = %llx\n", i, (uint64_t) readq(&vna_dsp_buffer[i]));
+      // vna_dsp_buffer[i] = 0xDEAD;
     }
   writeq(dmahandle_buffer, &bar0base[101]);
   udelay(1000);
@@ -117,7 +118,7 @@ static int pciesdr_probe(struct pci_dev *dev, const struct pci_device_id *id){
   udelay(1000);
   for(i=0; i<32; i++)
     {
-      printk("dmabuf[%d] = %llx\n", i, (uint64_t) readq(&pciesdr_buffer[i]));
+      printk("dmabuf[%d] = %llx\n", i, (uint64_t) readq(&vna_dsp_buffer[i]));
     }
   for(i=0; i<16; i++)
     {
@@ -130,56 +131,57 @@ static int pciesdr_probe(struct pci_dev *dev, const struct pci_device_id *id){
       //if((readq(&bar0base[i]) & (uint64_t) 0x0FFFFFFFF) != 0xDEADBEEF)
       printk("bar0[4] = %llx\n", (uint64_t) readq(&bar0base[3]));
     }
+  writeq(dmahandle_buffer, &bar0base[102]);
   return 0;
 }
 
-static void pciesdr_remove(struct pci_dev *dev){
+static void vna_dsp_remove(struct pci_dev *dev){
   iounmap(bar0base);
   free_irq(dev->irq, dev); // void
   pci_disable_msi(dev); // check retval?
-  pci_free_consistent(dev, 4*1024*1024, pciesdr_buffer, dmahandle_buffer);
+  pci_free_consistent(dev, 4*1024*1024, vna_dsp_buffer, dmahandle_buffer);
   pci_release_regions(dev);
   pci_disable_device(dev);
 }
 
-static struct pci_driver pciesdr_driver = {
-  .name = "pciesdr",
-  .id_table = pciesdr_pci_table,
-  .probe = pciesdr_probe,
-  .remove = pciesdr_remove,
+static struct pci_driver vna_dsp_driver = {
+  .name = "vna_dsp",
+  .id_table = vna_dsp_pci_table,
+  .probe = vna_dsp_probe,
+  .remove = vna_dsp_remove,
 };
 
 static struct class *vna_class;
 
-static int __init pciesdr_init(void){
-  printk ("Loading pciesdr kernel module\n");
-  pciesdr_major = register_chrdev(0, DEVICE_NAME, &fops);
-  if(pciesdr_major < 0){
-    printk(KERN_ALERT "Registering char device failed with %d\n", pciesdr_major);
-    return pciesdr_major;
+static int __init vna_dsp_init(void){
+  printk ("Loading vna_dsp kernel module\n");
+  vna_dsp_major = register_chrdev(0, DEVICE_NAME, &fops);
+  if(vna_dsp_major < 0){
+    printk(KERN_ALERT "Registering char device failed with %d\n", vna_dsp_major);
+    return vna_dsp_major;
   }
-  printk ("Assigned major = %d\n", pciesdr_major);
+  printk ("Assigned major = %d\n", vna_dsp_major);
   vna_class = class_create(THIS_MODULE, "vna");
   if (IS_ERR(vna_class)) {
     printk(KERN_ERR "Error creating vna class.\n");
     //goto error;
   }
-  device_create(vna_class, NULL, MKDEV(pciesdr_major, 0), NULL, "vna");
+  device_create(vna_class, NULL, MKDEV(vna_dsp_major, 0), NULL, "vna");
   
-  //devfs_register_chrdev(pciesdr_major, DEVICE_NAME, &fops);
-  return pci_register_driver(&pciesdr_driver);
+  //devfs_register_chrdev(vna_dsp_major, DEVICE_NAME, &fops);
+  return pci_register_driver(&vna_dsp_driver);
 }
 
-static void __exit pciesdr_exit(void){
-  pci_unregister_driver(&pciesdr_driver);
-  unregister_chrdev(pciesdr_major, DEVICE_NAME);
-  device_destroy(vna_class, MKDEV(pciesdr_major, 0));
+static void __exit vna_dsp_exit(void){
+  pci_unregister_driver(&vna_dsp_driver);
+  unregister_chrdev(vna_dsp_major, DEVICE_NAME);
+  device_destroy(vna_class, MKDEV(vna_dsp_major, 0));
   class_destroy(vna_class);
-  printk ("Unloading pciesdr kernel module.\n");
+  printk ("Unloading vna_dsp kernel module.\n");
   return;
 }
 
-module_init(pciesdr_init);
-module_exit(pciesdr_exit);
+module_init(vna_dsp_init);
+module_exit(vna_dsp_exit);
 
 MODULE_LICENSE("GPL");
