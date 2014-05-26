e910a99903745d08c846e78d93ce5b8584e25feb
Darrell Harmon <darrell@harmoninstruments.com>
2014-05-26 10:40:27 -0600

    add multi buffer allocation

--- vna_dsp.c
+++ vna_dsp.c
@@ -17,8 +17,16 @@ static struct pci_device_id vna_dsp_pci_table[] = {
   {0,}
 };
 
-static dma_addr_t dmahandle_buffer;
-static uint64_t *vna_dsp_buffer;
+#define BUFFER_PAGES_OUT 4
+#define BUFFER_PAGES_IN 16
+#define BUFFER_PAGE_SIZE (4096*1024) // this is the size of a page in the card's page table
+
+static dma_addr_t buffer_out_dma_addr[BUFFER_PAGES_OUT];
+static uint64_t *buffer_out[BUFFER_PAGES_OUT];
+
+static dma_addr_t buffer_in_dma_addr[BUFFER_PAGES_IN];
+static uint64_t *buffer_in[BUFFER_PAGES_IN];
+
 volatile static uint64_t *bar0base = 0;
 static int vna_dsp_major = 0;
 static int vna_dsp_is_open = 0;
@@ -44,8 +52,8 @@ static ssize_t vna_dsp_read(struct file *filp,/* see include/linux/fs.h   */
   unsigned long bytes_left;
   if(length > 4*1024*1024)
     length = 4*1024*1024;
-  bytes_left = copy_to_user(buffer, vna_dsp_buffer, length);
-  return length - bytes_left;
+  //bytes_left = copy_to_user(buffer, vna_dsp_buffer, length);
+  return 0;//length - bytes_left;
 }
 
 static ssize_t vna_dsp_write(struct file *filp,
@@ -68,13 +76,12 @@ static irqreturn_t vna_interrupt(int irq, void *dev_id, struct pt_regs *regs){
   //struct sample_dev *dev = dev_id;
   /* now `dev' points to the right hardware item */
   /* .... */
-  printk("VNA interrupt\n");
+  printk("VNA interrupt: sr = %llx\n", (uint64_t) readq(&bar0base[0]));
   return IRQ_HANDLED;
 }
 
-
 static int vna_dsp_probe(struct pci_dev *dev, const struct pci_device_id *id){
-  int i;
+  int i, j;
   int retval = pci_enable_device(dev);
   if(retval < 0) return retval;
   retval = pci_request_regions(dev, DEVICE_NAME);
@@ -82,64 +89,100 @@ static int vna_dsp_probe(struct pci_dev *dev, const struct pci_device_id *id){
     pci_disable_device(dev);
     return retval;
   }
+  printk("Found Harmon Instruments PCI Express interface board\n");
+
   pci_set_master  (dev);
   pci_set_dma_mask(dev, 0xFFFFFFFFFFFFFFFF); // if this fails, try 32
   pci_set_consistent_dma_mask(dev, 0xFFFFFFFFFFFFFFFF);
+
+  //pci_set_dma_mask(dev, 0xFFFFFFFF); // if this fails, try 32
+  //pci_set_consistent_dma_mask(dev, 0xFFFFFFFF);
+  
+  for(i=0; i<BUFFER_PAGES_OUT; i++){
+    buffer_out[i] = pci_alloc_consistent(dev, BUFFER_PAGE_SIZE, &buffer_out_dma_addr[i]);
+    printk("allocated buffer_out[%d] at %16llx phys: %16llx\n", i, (uint64_t) buffer_out[i], buffer_out_dma_addr[i]);
+    if(buffer_out[i] == NULL)
+      printk("Failed to allocate out buffer %d\n", i);
+    else{
+      for(j=0; j<BUFFER_PAGE_SIZE/8; j++)
+	buffer_out[i][j] = 0xDEADBEEF00000000 + j + i*512*1024;
+    }
+  }
+  for(i=0; i<BUFFER_PAGES_IN; i++){
+    buffer_in[i] = pci_alloc_consistent(dev, BUFFER_PAGE_SIZE, &buffer_in_dma_addr[i]);
+    printk("allocated buffer_in[%d] at %llx phys: %llx\n", i, (uint64_t) buffer_in[i], buffer_in_dma_addr[i]);
+    if(buffer_in[i] == NULL)
+      printk("Failed to allocate in buffer %d\n", i);
+    else{
+      for(j=0; j<BUFFER_PAGE_SIZE/8; j++)
+	buffer_in[i][j] = 0xDEADBEEF0BADC0DE;
+    }
+  }
+
   if(pci_enable_msi(dev) < 0)
     printk("VNA: pci_enable_msi() failed\n"); // check retval
   if(request_irq(dev->irq, (irq_handler_t) vna_interrupt, 0 /* flags */, DEVICE_NAME, dev) != 0)
     printk("VNA: request_irq() failed\n");
-  vna_dsp_buffer = (uint64_t *) pci_alloc_consistent(dev, 4*1024*1024, &dmahandle_buffer);
-  if(vna_dsp_buffer == NULL)
-    printk("Failed to allocate rx buffer\n");
-  printk("Allocated a buffer at %llx %llx\n", (uint64_t) vna_dsp_buffer, (uint64_t) dmahandle_buffer);
-  printk("Found PCI Express interface board\n");
+  
   bar0base = (uint64_t *) ioremap(pci_resource_start(dev, 0), 65536);
   printk("pci_resource_start(dev, 0) = %llx\n", (uint64_t) pci_resource_start(dev, 0));
   printk("BAR 0 base (remapped) = %llx\n", (uint64_t) bar0base);
-  writeq(0x0BADC0DE, &bar0base[1]);
-  udelay(1000);
-  for(i=0; i<15; i++)
-    {
-      printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&bar0base[i]));
-      writeq(i, &bar0base[15]);
-    }
-  writeq(0xDEADBEEF0BADC0DE, &bar0base[0]);
-  for(i=0; i<256; i++)
-    vna_dsp_buffer[i] = 0xBEEF00000000 | i;
+  
   for(i=0; i<8; i++)
-    {
-      printk("dmabuf[%d] = %llx\n", i, (uint64_t) readq(&vna_dsp_buffer[i]));
-      // vna_dsp_buffer[i] = 0xDEAD;
-    }
-  writeq(dmahandle_buffer, &bar0base[101]);
+    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&bar0base[i]));
+  
+  // set in match
+  writeq(0x1000 >> 2, &bar0base[10]);
+  // set out match
+  writeq(4096, &bar0base[13]); 
+  // enable interrupts
+  writeq(0xF, &bar0base[0]);
+  writeq(0, &bar0base[12]);
+  // load the page tables
+  for(i=0; i<32; i++){
+    writeq(buffer_out_dma_addr[0/*i%BUFFER_PAGES_OUT*/], &bar0base[i+512]);
+    writeq(buffer_in_dma_addr[0/*i%BUFFER_PAGES_IN*/], &bar0base[i+64]);
+  }
+  for(i=0; i<8; i++)
+    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&bar0base[i]));
+  // disable it
+  writeq(0, &bar0base[9]);
   udelay(1000);
-  writeq(dmahandle_buffer, &bar0base[100]);
+  // enable it
+  writeq(0, &bar0base[8]);
+  // write some data to the FIFO
   udelay(1000);
+  
+  for(i=0; i<8; i++)
+    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&bar0base[i]));
   for(i=0; i<32; i++)
-    {
-      printk("dmabuf[%d] = %llx\n", i, (uint64_t) readq(&vna_dsp_buffer[i]));
-    }
-  for(i=0; i<16; i++)
-    {
-      //if((readq(&bar0base[i]) & (uint64_t) 0x0FFFFFFFF) != 0xDEADBEEF)
-      printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&bar0base[i]));
-    }
-  for(i=0; i<16; i++)
-    {
-      writeq(0x1234, &bar0base[0]);
-      //if((readq(&bar0base[i]) & (uint64_t) 0x0FFFFFFFF) != 0xDEADBEEF)
-      printk("bar0[4] = %llx\n", (uint64_t) readq(&bar0base[3]));
-    }
-  writeq(dmahandle_buffer, &bar0base[102]);
+    printk("buf_in[%d] = %llx\n", i, (uint64_t) readq(&buffer_in[0][i]));
+  for(i=0; i<8; i++)
+    printk("buf_out[%d] = %llx\n", i, (uint64_t) readq(&buffer_out[0][i]));
+  
+  buffer_out[0][0] = 0x1EADBEEF00000000;
+  writeq(8192, &bar0base[11]); // enable
+  udelay(1000);
+  for(i=0; i<64; i++)
+    printk("buf_in[%d] = %llx\n", i, (uint64_t) readq(&buffer_in[0][i]));
+  for(i=0; i<8; i++)
+    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&bar0base[i]));
   return 0;
 }
 
 static void vna_dsp_remove(struct pci_dev *dev){
+  int i;
   iounmap(bar0base);
   free_irq(dev->irq, dev); // void
   pci_disable_msi(dev); // check retval?
-  pci_free_consistent(dev, 4*1024*1024, vna_dsp_buffer, dmahandle_buffer);
+  for(i=0; i<BUFFER_PAGES_OUT; i++){
+    if(buffer_out[i])
+      pci_free_consistent(dev, BUFFER_PAGE_SIZE, (void *) buffer_out[i], buffer_out_dma_addr[i]);
+  }
+  for(i=0; i<BUFFER_PAGES_IN; i++){
+    if(buffer_in[i])
+      pci_free_consistent(dev, BUFFER_PAGE_SIZE, (void *) buffer_in[i], buffer_in_dma_addr[i]);
+  }
   pci_release_regions(dev);
   pci_disable_device(dev);
 }
@@ -160,7 +203,6 @@ static int __init vna_dsp_init(void){
     printk(KERN_ALERT "Registering char device failed with %d\n", vna_dsp_major);
     return vna_dsp_major;
   }
-  printk ("Assigned major = %d\n", vna_dsp_major);
   vna_class = class_create(THIS_MODULE, "vna");
   if (IS_ERR(vna_class)) {
     printk(KERN_ERR "Error creating vna class.\n");
@@ -168,7 +210,6 @@ static int __init vna_dsp_init(void){
   }
   device_create(vna_class, NULL, MKDEV(vna_dsp_major, 0), NULL, "vna");
   
-  //devfs_register_chrdev(vna_dsp_major, DEVICE_NAME, &fops);
   return pci_register_driver(&vna_dsp_driver);
 }
 
