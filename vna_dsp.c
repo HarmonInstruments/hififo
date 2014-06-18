a7e193cac1e33bdcb9fcfa8ff70dce4be6144977
Darrell Harmon <darrell@harmoninstruments.com>
2014-06-18 14:53:36 -0600

    add to_pc circular buffer, misc

--- vna_dsp.c
+++ vna_dsp.c
@@ -19,29 +19,31 @@ static struct pci_device_id vna_dsp_pci_table[] = {
   {0,}
 };
 
-#define BUFFER_PAGES_FROM_PC 4
-#define BUFFER_PAGES_TO_PC 16
+#define BUFFER_PAGES_FROM_PC 1
+#define BUFFER_PAGES_TO_PC 1
 #define BUFFER_MAX_PAGES 32
 #define BUFFER_PAGE_SIZE (4096*1024) // this is the size of a page in the card's page table
+#define BUFFER_SIZE_TO_PC (BUFFER_PAGE_SIZE*BUFFER_PAGES_TO_PC)
+#define BUFFER_SIZE_FROM_PC (BUFFER_PAGE_SIZE*BUFFER_PAGES_FROM_PC)
 
 #define REG_INTERRUPT 0
 
-#define REG_TO_PC_ENABLE 8
-#define REG_TO_PC_DISABLE 9
-#define REG_TO_PC_MATCH 10
+#define REG_TO_PC_COUNT 3
+#define REG_TO_PC_STOP 8
+#define REG_TO_PC_MATCH 9
+#define REG_TO_PC_ENABLE 10
 #define REG_FROM_PC_ENABLE 11
 #define REG_FROM_PC_DISABLE 12
 #define REG_FROM_PC_MATCH 13
 #define REG_FROM_PC_PAGE_TABLE_BASE 64
 #define REG_TO_PC_PAGE_TABLE_BASE 512
 
-static int vna_dsp_major = 0;
-
 static struct class *vna_class;
 
 struct hififo_dev {
   struct cdev hififo_cdev;
   dma_addr_t to_pc_dma_addr[BUFFER_MAX_PAGES];
+  u64 to_pc_pointer;
   dma_addr_t from_pc_dma_addr[BUFFER_MAX_PAGES];
   u64 *to_pc_buffer[BUFFER_MAX_PAGES];
   u64 *from_pc_buffer[BUFFER_MAX_PAGES];
@@ -49,24 +51,28 @@ struct hififo_dev {
   wait_queue_head_t queue_read;
   int is_open;
   int a;
+  int major;
 };
 
+#define fifo_writereg(data, addr) (writeq(cpu_to_le64(data), &drvdata->pio_reg_base[addr]))
+#define fifo_readreg(addr) le64_to_cpu(readq(&drvdata->pio_reg_base[addr]))
+
 static int vna_dsp_open(struct inode *inode, struct file *filp){
-  struct hififo_dev *dev = container_of(inode->i_cdev, struct hififo_dev, hififo_cdev);
-  filp->private_data = dev;
-  printk("hififo: open, a = %d\n", dev->a);
-  if (dev->is_open)
+  struct hififo_dev *drvdata = container_of(inode->i_cdev, struct hififo_dev, hififo_cdev);
+  filp->private_data = drvdata;
+  printk("hififo: open, a = %d\n", drvdata->a);
+  if (drvdata->is_open)
     return -EBUSY;
-  dev->is_open++;
+  drvdata->is_open++;
   try_module_get(THIS_MODULE); // increment the usage count
   return SUCCESS;
 }
 
 static int vna_dsp_release(struct inode *inode, struct file *filp){
-  struct hififo_dev *dev = filp->private_data;
-  dev->is_open--;/* We're now ready for our next caller */
+  struct hififo_dev *drvdata = filp->private_data;
+  drvdata->is_open--;/* We're now ready for our next caller */
   module_put(THIS_MODULE); // decrement the usage count
-  printk("hififo: close, a = %d\n", dev->a);
+  printk("hififo: close, a = %d\n", drvdata->a);
   return SUCCESS;
 }
 
@@ -74,15 +80,38 @@ static ssize_t vna_dsp_read(struct file *filp,
 			    char *buffer,
 			    size_t length,
 			    loff_t * offset){
-  struct hififo_dev *dev = filp->private_data;
-  length &= 0xFFFFFFFFF8;
-  if(length > 1024)
-    length = 1024;
+  struct hififo_dev *drvdata = filp->private_data;
+  size_t count = 0;
+  u64 bytes_in_buffer;
+  u64 copy_length;
+  u64 max_copy_length;
+  int retval;
+  printk(KERN_INFO DEVICE_NAME ": read, %d bytes\n", (int) length);
   if((length&0x7) != 0) /* reads must be a multiple of 8 bytes */
     return -EINVAL;
-  copy_to_user(buffer, dev->to_pc_buffer[0], length);
-  //status = wait_event_interruptible_timeout(queue_read, condition, HZ/4); // retval: 0 if timed out, else number of jiffies remaining
-  printk("hififo: read, a = %d, %d bytes\n", dev->a++, length);
+  while(count != length){
+    copy_length = BUFFER_PAGE_SIZE/2 - (drvdata->to_pc_pointer & (BUFFER_PAGE_SIZE/2-1));
+    if(copy_length > (length-count))
+      copy_length = (length-count);
+    bytes_in_buffer = (fifo_readreg(REG_TO_PC_COUNT) - drvdata->to_pc_pointer) & (BUFFER_SIZE_TO_PC - 1);
+    printk("%lld bytes in buffer, copy length = %lld\n", bytes_in_buffer, copy_length);
+    if(copy_length > bytes_in_buffer){
+      fifo_writereg(drvdata->to_pc_pointer + copy_length, REG_TO_PC_MATCH);
+      retval = wait_event_interruptible_timeout(drvdata->queue_read, (((fifo_readreg(REG_TO_PC_COUNT) - drvdata->to_pc_pointer) & (BUFFER_SIZE_TO_PC - 1)) >= copy_length), HZ/4); // retval: 0 if timed out, else number of jiffies remaining
+      printk("wait retval = %d\n", retval);
+      bytes_in_buffer = (fifo_readreg(REG_TO_PC_COUNT) - drvdata->to_pc_pointer) & (BUFFER_SIZE_TO_PC - 1);
+      printk("%lld bytes in buffer, copy length = %lld\n", bytes_in_buffer, copy_length);
+    }
+    retval = copy_to_user(buffer+count, (drvdata->to_pc_pointer & (BUFFER_SIZE_TO_PC -1)) + (char *) drvdata->to_pc_buffer[0], copy_length);
+    if(retval != 0){
+      printk(KERN_ERR DEVICE_NAME "copy_to_user failed with %d\n", retval);
+      return -EFAULT;
+    }
+    count += copy_length;
+    drvdata->to_pc_pointer += copy_length;
+    fifo_writereg(drvdata->to_pc_pointer + (BUFFER_SIZE_TO_PC - 128), REG_TO_PC_STOP);
+  }
+  
   return length;
 }
 
@@ -90,16 +119,16 @@ static ssize_t vna_dsp_write(struct file *filp,
 			    const char *buf,
 			    size_t length,
 			    loff_t * off){
-  struct hififo_dev *dev = filp->private_data;
+  struct hififo_dev *drvdata = filp->private_data;
   int retval;
-  printk("hififo: write, a = %d, %d bytes\n", dev->a++, length);
+  printk("hififo: write, a = %d, %d bytes\n", drvdata->a++, (int) length);
   length &= 0xFFFFFFFFF8;
   if((length&0x7) != 0) /* writes must be a multiple of 8 bytes */
     return -EINVAL;
   if(length > (4*1024*1024))
     length = 4*1024*1024;
-  retval = copy_from_user(dev->from_pc_buffer[0], buf, length);
-  writeq(length >> 3, &dev->pio_reg_base[REG_FROM_PC_ENABLE]);
+  retval = copy_from_user(drvdata->from_pc_buffer[0], buf, length);
+  fifo_writereg(length >> 3, REG_FROM_PC_ENABLE);
   return length;//-EINVAL;
 }
 
@@ -111,80 +140,59 @@ static struct file_operations fops = {
 };
 
 static irqreturn_t vna_interrupt(int irq, void *dev_id, struct pt_regs *regs){
-  struct hififo_dev *dev = dev_id;
-  wake_up_all(&dev->queue_read);
-  printk("VNA interrupt: sr = %llx, a = %d\n", (uint64_t) readq(&dev->pio_reg_base[REG_INTERRUPT]), dev->a);
+  struct hififo_dev *drvdata = dev_id;
+  wake_up_all(&drvdata->queue_read);
+  printk("VNA interrupt: sr = %llx\n", (uint64_t) fifo_readreg(REG_INTERRUPT));
   return IRQ_HANDLED;
 }
 
-static void check_buffer(u64 *data[])
-{
-  int i;
-  uint64_t prev = 0;
-  uint64_t cur;
-  int printcount = 0;
-  for(i=0; i<512*1024; i++){
-    cur = readq(&data[0][i]);
-    if(cur != prev + 1){
-      if(printcount < 200)
-	printk("buf_in[%d] = %llx\n", i, (uint64_t) cur);
-      printcount++;
-    }
-    prev = cur;
-  }
-  printk("found %d errors\n", printcount);
-}
-
 static int vna_dsp_probe(struct pci_dev *pdev, const struct pci_device_id *id){
-  int i, j, err, devno;
+  int i, j, err;
   int retval;
   dev_t dev = 0;
   struct hififo_dev *drvdata;
 
-  retval = alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME);
-  vna_dsp_major = MAJOR(dev);
-  if (retval < 0) {
-    printk(KERN_WARNING "vna_dsp: can't get major %d\n", vna_dsp_major);
-    return retval;
-  }
   drvdata = devm_kzalloc(&pdev->dev, sizeof(struct hififo_dev), GFP_KERNEL);
   if (!drvdata){
     printk(KERN_ERR "failed to alloc drvdata\n");
     return -ENOMEM;
   }
-  devno = MKDEV(vna_dsp_major, 0);
+
+  retval = alloc_chrdev_region(&dev, 0, 1, DEVICE_NAME);
+  if (retval < 0) {
+    printk(KERN_ERR "vna_dsp: can't get major %d\n", MAJOR(dev));
+    return retval;
+  }
+  
+  drvdata->major = MAJOR(dev);
   cdev_init(&drvdata->hififo_cdev, &fops);
   drvdata->hififo_cdev.owner = THIS_MODULE;
   drvdata->hififo_cdev.ops = &fops;
-  err = cdev_add (&drvdata->hififo_cdev, devno, 1);
+  err = cdev_add (&drvdata->hififo_cdev, MKDEV(MAJOR(dev), 0), 1);
   if (err)
     printk(KERN_NOTICE "Error %d adding cdev\n", err);
-  device_create(vna_class, NULL, MKDEV(vna_dsp_major, 0), NULL, "vna");
-
+  device_create(vna_class, NULL, MKDEV(MAJOR(dev), 0), NULL, "vna");
   
   init_waitqueue_head(&drvdata->queue_read);
   drvdata->a = 3;
 
   retval = pcim_enable_device(pdev);
-  if(retval < 0) return retval;
+  if(retval < 0) 
+    return retval;
   retval = pci_request_regions(pdev, DEVICE_NAME);
   if(retval < 0){
     pci_disable_device(pdev);
     return retval;
   }
-  printk("Found Harmon Instruments PCI Express interface board\n");
+  printk(DEVICE_NAME ": Found Harmon Instruments PCI Express interface board\n");
   pci_set_drvdata(pdev, drvdata);
-
   pci_set_master(pdev); // check return values on these
   pci_set_dma_mask(pdev, 0xFFFFFFFFFFFFFFFF);
   pci_set_consistent_dma_mask(pdev, 0xFFFFFFFFFFFFFFFF);
 
-  //pci_set_dma_mask(dev, 0xFFFFFFFF); // if this fails, try 32
-  //pci_set_consistent_dma_mask(dev, 0xFFFFFFFF);
-   
   for(i=0; i<BUFFER_PAGES_FROM_PC; i++){
     drvdata->from_pc_buffer[i] = pci_alloc_consistent(pdev, BUFFER_PAGE_SIZE, &drvdata->from_pc_dma_addr[i]);
-    printk("allocated drvdata->from_pc_buffer[%d] at %16llx phys: %16llx\n", i, (u64) drvdata->from_pc_buffer[i], drvdata->from_pc_dma_addr[i]);
+    //printk("allocated drvdata->from_pc_buffer[%d] at %16llx phys: %16llx\n", i, (u64) drvdata->from_pc_buffer[i], drvdata->from_pc_dma_addr[i]);
     if(drvdata->from_pc_buffer[i] == NULL){
       printk("Failed to allocate out buffer %d\n", i);
       return -ENOMEM;
@@ -194,9 +202,10 @@ static int vna_dsp_probe(struct pci_dev *pdev, const struct pci_device_id *id){
 	drvdata->from_pc_buffer[i][j] = j + i*512*1024;
     }
   }
+
   for(i=0; i<BUFFER_PAGES_TO_PC; i++){
     drvdata->to_pc_buffer[i] = pci_alloc_consistent(pdev, BUFFER_PAGE_SIZE, &drvdata->to_pc_dma_addr[i]);
-    printk("allocated drvdata->to_pc_buffer[%d] at %llx phys: %llx\n", i, (u64) drvdata->to_pc_buffer[i], drvdata->to_pc_dma_addr[i]);
+    //printk("allocated drvdata->to_pc_buffer[%d] at %llx phys: %llx\n", i, (u64) drvdata->to_pc_buffer[i], drvdata->to_pc_dma_addr[i]);
     if(drvdata->to_pc_buffer[i] == NULL){
       printk("Failed to allocate in buffer %d\n", i);
       return -ENOMEM;
@@ -207,62 +216,60 @@ static int vna_dsp_probe(struct pci_dev *pdev, const struct pci_device_id *id){
     }
   }
 
-  if(pci_enable_msi(pdev) < 0){
-    printk(DEVICE_NAME ": pci_enable_msi() failed\n");
-    return -1;
+  retval = pci_enable_msi(pdev);
+  if(retval < 0){
+    printk(KERN_ERR DEVICE_NAME ": pci_enable_msi() failed\n");
+    return retval;
   }
-  if(devm_request_irq(&pdev->dev, pdev->irq, (irq_handler_t) vna_interrupt, 0 /* flags */, DEVICE_NAME, drvdata) != 0){
-    printk(DEVICE_NAME ": request_irq() failed\n");
-    return -1;
-  }
-  drvdata->pio_reg_base = (uint64_t *) pcim_iomap(pdev, 0, 65536);
-  printk("pci_resource_start(dev, 0) = %llx\n", (u64) pci_resource_start(pdev, 0));
-  printk("BAR 0 base (remapped) = %llx\n", (u64) drvdata->pio_reg_base);
 
+  retval = devm_request_irq(&pdev->dev, pdev->irq, (irq_handler_t) vna_interrupt, 0 /* flags */, DEVICE_NAME, drvdata);
+  if(retval){
+    printk(KERN_ERR DEVICE_NAME ": request_irq() failed\n");
+    return retval;
+  }
+
+  drvdata->pio_reg_base = (u64 *) pcim_iomap(pdev, 0, 65536);
+  printk(KERN_INFO DEVICE_NAME ": pci_resource_start(dev, 0) = 0x%.8llx, virt = 0x%.16llx\n", (u64) pci_resource_start(pdev, 0), (u64) drvdata->pio_reg_base);
+  
   for(i=0; i<8; i++)
-    printk("buf_in[%d] = %llx\n", i, (uint64_t) readq(&drvdata->to_pc_buffer[0][i]));
-  for(i=0; i<8; i++)
-    printk("buf_out[%d] = %llx\n", i, (uint64_t) readq(&drvdata->from_pc_buffer[0][i]));
-  for(i=0; i<8; i++)
-    printk("bar0[%d] = %llx\n", i, (u64) readq(&drvdata->pio_reg_base[i]));
+    printk("bar0[%d] = %llx\n", i, (u64) fifo_readreg(i));
 
   /* disable it */
-  writeq(cpu_to_le64(0x0), &drvdata->pio_reg_base[REG_FROM_PC_DISABLE]);
-  writeq(cpu_to_le64(0x0), &drvdata->pio_reg_base[REG_TO_PC_DISABLE]);
+  fifo_writereg(0, REG_FROM_PC_DISABLE);
+  fifo_writereg(0, REG_TO_PC_ENABLE);
+  udelay(10); /* wait for completion of anything that was running */
   /* set interrupt match registers */
-  writeq(0x1000 >> 2, &drvdata->pio_reg_base[REG_TO_PC_MATCH]);
-  writeq(256*1024, &drvdata->pio_reg_base[REG_FROM_PC_MATCH]); 
+  fifo_writereg(0, REG_TO_PC_MATCH);
+  fifo_writereg(0, REG_FROM_PC_MATCH); 
   /* enable interrupts */
-  writeq(cpu_to_le64(0xF), &drvdata->pio_reg_base[REG_INTERRUPT]);
+  fifo_writereg(0xF, REG_INTERRUPT);
   /* load the card page tables */
   for(i=0; i<32; i++){
-    writeq(cpu_to_le64(drvdata->to_pc_dma_addr[i%BUFFER_PAGES_TO_PC]), &drvdata->pio_reg_base[i+REG_TO_PC_PAGE_TABLE_BASE]);
-    writeq(cpu_to_le64(drvdata->from_pc_dma_addr[i%BUFFER_PAGES_FROM_PC]), &drvdata->pio_reg_base[i+REG_FROM_PC_PAGE_TABLE_BASE]);
+    fifo_writereg(drvdata->to_pc_dma_addr[i%BUFFER_PAGES_TO_PC], i+REG_TO_PC_PAGE_TABLE_BASE);
+    fifo_writereg(drvdata->from_pc_dma_addr[i%BUFFER_PAGES_FROM_PC], i+REG_FROM_PC_PAGE_TABLE_BASE);
   }
   for(i=0; i<8; i++)
-    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&drvdata->pio_reg_base[i]));
+    printk("bar0[%d] = %llx\n", i, (u64) fifo_readreg(i));
   /* enable it */
-  writeq(0x1000, &drvdata->pio_reg_base[REG_TO_PC_ENABLE]);
-  writeq(4*1024*1024, &drvdata->pio_reg_base[REG_FROM_PC_ENABLE]); // enable
+  fifo_writereg(BUFFER_SIZE_TO_PC-128, REG_TO_PC_STOP);
+  fifo_writereg(1, REG_TO_PC_ENABLE);
+  fifo_writereg(4*1024*1024, REG_FROM_PC_ENABLE); // enable
+  drvdata->to_pc_pointer = 0;
   udelay(1000);
   for(i=0; i<8; i++)
-    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&drvdata->pio_reg_base[i]));
+    printk("bar0[%d] = %llx\n", i, (u64) fifo_readreg(i));
   udelay(1000);
   for(i=0; i<8; i++)
-    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&drvdata->pio_reg_base[i]));
-  for(i=0; i<32; i++)
-    printk("buf_in[%d] = %llx\n", i, (uint64_t) readq(&drvdata->to_pc_buffer[0][i]));
+    printk("bar0[%d] = %llx\n", i, (u64) fifo_readreg(i));
   udelay(10000);
-  printk("check buf\n");
-  check_buffer(drvdata->from_pc_buffer);
   for(i=0; i<8; i++)
-    printk("bar0[%d] = %llx\n", i, (uint64_t) readq(&drvdata->pio_reg_base[i]));
+    printk("bar0[%d] = %llx\n", i, (u64) fifo_readreg(i));
   return 0;
 }
 
 static void vna_dsp_remove(struct pci_dev *pdev){
-  //unregister_chrdev(vna_dsp_major, DEVICE_NAME);
-  device_destroy(vna_class, MKDEV(vna_dsp_major, 0));
+  struct hififo_dev *drvdata = pci_get_drvdata(pdev);
+  device_destroy(vna_class, MKDEV(drvdata->major, 0));
 }
 
 static struct pci_driver vna_dsp_driver = {
