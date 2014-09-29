d7b78d88fe8d790b9bfb644434c3f22567c7203c
Darrell Harmon <darrell@harmoninstruments.com>
2014-09-29 11:27:21 -0600

    clean up copyright notice

--- hdl/sequencer.v
+++ hdl/sequencer.v
@@ -1,6 +1,7 @@
 /* 
  * HIFIFO: Harmon Instruments PCI Express to FIFO
  * Copyright (C) 2014 Harmon Instruments, LLC
+ * Author: Darrell Harmon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,12 +15,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/
- */
-
-/*
- * Copyright 2014 Harmon Instruments, LLC
- * Author: Darrell Harmon
- *
  */
 
 module sequencer
