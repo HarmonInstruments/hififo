4c9ce1caa40a28e5cd7cdb48c316d2fb9f035674
Darrell Harmon <darrell@harmoninstruments.com>
2014-10-12 17:50:20 -0600

    add SPI class

---
+++ user/Spi_Config.cpp
@@ -0,0 +1,62 @@
+/*
+ * HIFIFO: Harmon Instruments PCI Express to FIFO
+ * Copyright (C) 2014 Harmon Instruments, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <thread>
+#include <iostream>
+#include <stdexcept>
+
+#include "Spi_Config.h"
+#include "Sequencer.h"
+
+using namespace std;
+
+SPI_Config::SPI_Config(Sequencer *sequencer, int addr)
+{
+  seq = sequencer;
+  spi_address = addr;
+  cerr << "opened SPI_Config, addr = " << addr << "\n";
+}
+
+void SPI_Config::txrx(char * data, int len, int read_offset)
+{
+  cerr << "spi transaction, len = " << len << " read_offset = " << read_offset << "\n";
+  for(int i=0; i<len; i++){
+    cerr << "tdata[" << i << "] = " << (0xFF & ((unsigned int) data[i])) << "\n";
+    uint64_t d_next = 0xFF & data[i];
+    if(len != i+1)
+      d_next |= 0x100;
+    seq->write_single(spi_address, d_next);
+    seq->wait(2*128);
+    if((read_offset >= 0) && (i >= read_offset))
+      seq->read_req(1, spi_address);
+  }
+  if(read_offset < 0)
+    return;
+  uint64_t * rdata64 = seq->run();
+  for(int i=read_offset; i<len; i++)
+    {
+      data[i-read_offset] = rdata64[i];
+      cerr << "rdata[" << i << "] = " << rdata64[i] << "\n";
+    }
+}
+
---
+++ user/Spi_Config.h
@@ -0,0 +1,32 @@
+/* 
+ * HIFIFO: Harmon Instruments PCI Express to FIFO
+ * Copyright (C) 2014 Harmon Instruments, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ */
+
+#pragma once
+
+#include "Sequencer.h"
+
+using namespace std;
+
+class SPI_Config {
+ private:
+  Sequencer *seq;
+  int spi_address;
+ public:
+  SPI_Config(Sequencer *sequencer, int addr);
+  void txrx(char * data, int len, int read_offset);
+};
