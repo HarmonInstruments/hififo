c512d78d64ff0578f39b7a174a63eaa633bee03d
Darrell Harmon <darrell@harmoninstruments.com>
2014-10-12 21:42:35 -0600

    build with Vivado 2014.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source ~/software/Xilinx/Vivado/2014.2/settings64.sh
+source ~/software/Xilinx/Vivado/2014.3/settings64.sh
 rm -rf xbuild/*
 vivado -source init.tcl -mode tcl
 cp xbuild/pcie3.runs/impl_1/vna_dsp.bin top.bin
