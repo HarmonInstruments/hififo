5c094fafa09f878b6186cd795ec3e48e2de537d3
Darrell Harmon <darrell@harmoninstruments.com>
2014-10-30 19:22:55 -0600

    update bitstream filename

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 source ~/software/Xilinx/Vivado/2014.3/settings64.sh
 rm -rf xbuild/*
 vivado -source init.tcl -mode tcl
-cp xbuild/pcie3.runs/impl_1/vna_dsp.bin top.bin
+cp xbuild/hififo.runs/impl_1/vna_dsp.bin top.bin
