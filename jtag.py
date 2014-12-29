9c32779543919573d9069056a64ce21f5604249a
Darrell Harmon <darrell@harmoninstruments.com>
2014-12-29 16:52:34 -0700

    avoid xadc read on parts that don't have it

--- jtag.py
+++ jtag.py
@@ -6,13 +6,15 @@ import array
 import struct
 
 if __name__ == "__main__":
+    import sys
     jtag = XilinxJtag(0x0403, 0x6014, 1)
     jtag.get_idcode_from_reset()
-    jtag.get_idcode_from_instruction()
-    jtag.get_dna()
-    jtag.read_xadc()
-    jtag.get_idcode_from_instruction()
-    import sys
+    #jtag.get_idcode_from_instruction()
+    #jtag.get_dna()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'xadc':
+            jtag.read_xadc()
+    #jtag.get_idcode_from_instruction()
     if len(sys.argv) > 1:
         jtag.program(sys.argv[1])
     jtag.get_idcode_from_instruction()
